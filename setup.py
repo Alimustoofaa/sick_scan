7062198ebe3c2bb53dd763a7812201162f982a2c
Alimustoofaa <softhouse.ali@gmail.com>
2023-09-30 17:29:15 +0700

    setup

--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name = 'sick_scan_tcp',
     version = '1.0.0',
     description = 'Library for interacting with SICK LIDAR sensors with TCP Connection',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     author = 'Ali Mustofa',
     author_email = 'hai.alimustofa@gmail.com',
+    url = 'https://github.com/Alimustoofaa/sick_scan',
+    keywords = ['Sick Scan', 'Sick Lidar', 'LMS511', 'LMS111'],
     packages = find_packages(),
-    install_requires = requirements
+    install_requires = requirements,
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
